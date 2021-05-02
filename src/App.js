d3e87b293cc82e2ff70485d36f1119bc9abaf51f
vanderL <vnd.vander@gmail.com>
2021-05-02 20:51:43 -0300

    Config Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './routes'
+
 function App() {
   return (
-   <div>Estrutura Flux!</div>
+    <BrowserRouter>
+    {/** Header */}
+      <Routes />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Cart/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// import { Container } from './styles';
+
+export default function Cart() {
+    return (
+        <div>
+            Olá! Sou o carrinho
+            <Link to='/'> Home </Link>
+        </div>
+)
+}
+
