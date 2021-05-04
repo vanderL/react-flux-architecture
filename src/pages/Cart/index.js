e86ac22dd4cddd40623a382b3d35cba08846b7de
vanderL <vnd.vander@gmail.com>
2021-05-04 10:35:12 -0300

    Styling HomePage

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+// import {  } from './styles';
 
 export default function Cart() {
     return (
         <div>
             Olá! Sou o carrinho
+            <Link to="/"> Voltar para a home</Link>
         </div>
 )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,95 @@
 import React from 'react';
-// import { Container } from './styles';
+
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Olá Brasil! Sou a Home</h1>
-        </div>
+        <ProductList>
+            <li>
+                <img 
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
+                    alt="Tênis"
+                />
+                <strong>Texto demasiadamente gigantesco, justamente para testar o alinhamento e o encaixe e verificar conflito com o button</strong>
+                <span> R$ 79,90 </span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff"/>
+                    </div>
+                    
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+
+            <li>
+                <img 
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
+                    alt="Tênis"
+                />
+                <strong>Belo Tenis</strong>
+                <span> R$ 79,90 </span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff"/>
+                    </div>
+                    
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+
+            <li>
+                <img 
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
+                    alt="Tênis"
+                />
+                <strong>Belo Tenis</strong>
+                <span> R$ 79,90 </span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff"/>
+                    </div>
+                    
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+
+            <li>
+                <img 
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
+                    alt="Tênis"
+                />
+                <strong>Belo Tenis</strong>
+                <span> R$ 79,90 </span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff"/>
+                    </div>
+                    
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+
+            <li>
+                <img 
+                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
+                    alt="Tênis"
+                />
+                <strong>Belo Tenis</strong>
+                <span> R$ 79,90 </span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart color="#fff"/>
+                    </div>
+                    
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
     )
 }
