02fe01765810efb01e90c7e841102c7876cf1785
vanderL <vnd.vander@gmail.com>
2021-05-04 13:18:35 -0300

    Styling CartPage

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,14 +1,65 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-// import {  } from './styles';
+import { MdRemoveCircleOutline, MdDelete, MdAddCircleOutline } from 'react-icons/md'
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
     return (
-        <div>
-            Olá! Sou o carrinho
-            <Link to="/"> Voltar para a home</Link>
-        </div>
-)
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBOTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>
+                        <img 
+                            src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
+                            alt="Tênis"
+                        />
+                        </td>
+                        <td>
+                            <strong>Tênis muito massa</strong>
+                            <span>R$ 129.90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                </button>
+                                <input type="number" readOnly value={2}/>
+                                <button type="button">
+                                    <MdAddCircleOutline size={20} color="#7159c1" />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$: 258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button" > Finalizar pedido </button>
+            
+                <Total>
+                    <span>TOTAL</span>
+                    <strong> R$ 1920,28</strong>
+                </Total>
+            </footer>
+        </Container>
+    )
 }
 
