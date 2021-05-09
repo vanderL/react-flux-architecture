29e6ab054954fdc51a2bfb56e52d24e9a5361b0b
vanderL <vnd.vander@gmail.com>
2021-05-09 17:08:29 -0300

    Seeking products on API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,53 @@
-import React from 'react';
+import React, { Component} from 'react';
+
+import api from '../../services/api';
+import { formatPrice } from '../../util/format'
 
 import { MdAddShoppingCart } from 'react-icons/md'
 import { ProductList } from './styles';
 
-export default function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img 
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
-                    alt="Tênis"
-                />
-                <strong>Texto demasiadamente gigantesco, justamente para testar o alinhamento e o encaixe e verificar conflito com o button</strong>
-                <span> R$ 79,90 </span>
+export default class Home extends Component {
+    state = {
+        products: [],
+    }
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff"/>
-                    </div>
-                    
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
+    async componentDidMount() {
+        const response = await api.get('products');
 
-            <li>
-                <img 
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
-                    alt="Tênis"
-                />
-                <strong>Belo Tenis</strong>
-                <span> R$ 79,90 </span>
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff"/>
-                    </div>
-                    
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
 
-            <li>
-                <img 
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
-                    alt="Tênis"
-                />
-                <strong>Belo Tenis</strong>
-                <span> R$ 79,90 </span>
+        this.setState({ products: data });
+    }
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff"/>
-                    </div>
-                    
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
+    render(){
+        const { products } = this.state;
 
-            <li>
-                <img 
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
-                    alt="Tênis"
-                />
-                <strong>Belo Tenis</strong>
-                <span> R$ 79,90 </span>
+        return (
+            <ProductList>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img 
+                            src={product.image} 
+                            alt={product.title}
+                        />
+                        <strong>{product.title}</strong>
+                        <span> {product.priceFormatted} </span>
+        
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart color="#fff"/>
+                            </div>
+                            
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        )
+    }
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff"/>
-                    </div>
-                    
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img 
-                    src="https://static.netshoes.com.br/produtos/tenis-olympikus-hobby-feminino/06/D22-2978-006/D22-2978-006_zoom2.jpg?ts=1567444589&ims=326x" 
-                    alt="Tênis"
-                />
-                <strong>Belo Tenis</strong>
-                <span> R$ 79,90 </span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart color="#fff"/>
-                    </div>
-                    
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-        </ProductList>
-    )
 }
